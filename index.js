fa6111e7200cfa95b144104341d6aabc034141cd
p-skipper <kipperspedro@gmail.com>
2024-11-18 15:30:50 -0300

    feat: create function to calculate the inss

--- index.js
+++ index.js
@@ -13,13 +13,38 @@ const adicionarFuncionario = (id, nome, cargo, taxaHoraria) => {
 };
 
 const registrarHoras = (id, horasTrabalhadas) => {
-  const funcionario = funcionarios.find(func => func.id === id);
+  const funcionario = funcionarios.find((func) => func.id === id);
   if (funcionario) {
     funcionario.horasTrabalhadas.push(horasTrabalhadas);
   }
 };
 
-function calcularSalarioMensal(funcionario) {
-  const totalHoras = funcionario.horasTrabalhadas.reduce((total, hora) => total + hora, 0);
+const calcularSalarioMensal = (funcionario) => {
+  const totalHoras = funcionario.horasTrabalhadas.reduce(
+    (total, hora) => total + hora,
+    0
+  );
   return totalHoras * funcionario.taxaHoraria;
+};
+
+function calcularInss(funcionario) {
+  const salarioBruto = calcularSalarioMensal(funcionario);
+
+  const faixas = [
+    { limite: 1412.0, aliquota: 7.5 },
+    { limite: 2666.68, aliquota: 9 },
+    { limite: 4000.03, aliquota: 12 },
+    { limite: Infinity, aliquota: 14 },
+  ];
+  const tetoInss = 908.85;
+
+  let inss = 0;
+  for (let faixa of faixas) {
+    if (salarioBruto <= faixa.limite) {
+      inss = (salarioBruto * faixa.aliquota) / 100;
+      break;
+    }
+  }
+
+  return Math.min(inss, tetoInss);
 }
