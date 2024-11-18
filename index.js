91a68889df4bb208c66b8d4ca17216f265a42bce
p-skipper <kipperspedro@gmail.com>
2024-11-18 14:51:26 -0300

    feat: create funtion to register hours worked

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ const adicionarFuncionario = (id, nome, cargo, taxaHoraria) => {
 
   funcionarios.push(funcionario);
 };
+
+const registrarHoras = (id, horasTrabalhadas) => {
+  const funcionario = funcionarios.find(func => func.id === id);
+  if (funcionario) {
+    funcionario.horasTrabalhadas.push(horasTrabalhadas);
+  }
+};
